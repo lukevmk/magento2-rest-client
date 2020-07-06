6e8d163deef1271b3f4fcde648186163e349003a
Christiaan Mes <christiaan@ptchr.nl>
2020-07-06 13:26:55 +0200

    Add searching a customer by email

--- src/Client.php
+++ src/Client.php
@@ -2,10 +2,121 @@
 
 namespace Ptchr\Magento2RestClient;
 
+use Carbon\Carbon;
+use GuzzleHttp\Exception\GuzzleException;
+
 class Client
 {
+    /**
+     * @var string
+     */
+    private string $baseUrl;
+
+    /**
+     * @var string
+     */
+    private string $username;
+
+    /**
+     * @var string
+     */
+    private string $password;
+
     /**
      * @var \GuzzleHttp\Client
      */
     private $guzzle;
+
+    /**
+     * @var string
+     */
+    private string $apiPrefix = '/rest/V1/';
+
+    /**
+     * @var Carbon|null
+     */
+    private $authenticatedAt = null;
+
+    /**
+     * @var string
+     */
+    private $accessToken;
+
+    /**
+     * Client constructor.
+     * @param string $baseUrl
+     * @param string $username
+     * @param string $password
+     * @throws GuzzleException
+     */
+    public function __construct(string $baseUrl, string $username, string $password)
+    {
+        $this->baseUrl = $baseUrl;
+        $this->username = $username;
+        $this->password = $password;
+        $this->guzzle = new \GuzzleHttp\Client();
+        $this->authenticate();
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    private function authenticate(): void
+    {
+        $isAuthenticated = $this->authenticatedAt instanceof Carbon && Carbon::now()->gt($this->authenticatedAt->addHours(4));
+
+        if ($isAuthenticated) {
+            return;
+        }
+
+        $response = $this->guzzle->post($this->baseUrl . $this->apiPrefix . 'integration/admin/token', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'json' => [
+                'username' => $this->username,
+                'password' => $this->password,
+            ],
+        ]);
+
+        $this->authenticatedAt = Carbon::now();
+        $this->accessToken = str_replace('"', '', $response->getBody()->getContents());
+    }
+
+    /**
+     * @param string $email
+     * @return mixed
+     * @throws GuzzleException
+     */
+    public function searchCustomerByEmail(string $email): array
+    {
+        $this->authenticate();
+        $parameters = [
+            'searchCriteria' => [
+                'pageSize' => 1,
+                'filterGroups' => [
+                    [
+                        'filters' => [
+                            [
+                                'field' => 'email',
+                                'value' => $email,
+                            ],
+                        ],
+                    ],
+                ],
+
+            ],
+        ];
+
+        $response = $this->guzzle->get($this->baseUrl . $this->apiPrefix . 'customers/search', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->accessToken,
+                'Content-Type' => 'application/json',
+            ],
+
+            'query' => $parameters,
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
+    }
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,13 +2,34 @@
 
 namespace Ptchr\Magento2RestClient\Tests;
 
+use Dotenv\Dotenv;
+use GuzzleHttp\Exception\GuzzleException;
 use PHPUnit\Framework\TestCase;
+use Ptchr\Magento2RestClient\Client;
 
 class ExampleTest extends TestCase
 {
-    /** @test */
-    public function true_is_true()
+    public function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+
+        $dotEnv = Dotenv::createImmutable(__DIR__.'/../');
+        $dotEnv->load();
+    }
+
+    /**
+     * @test
+     * @throws GuzzleException
+     */
+    public function searching_a_customer()
+    {
+        $client = new Client(
+            $_ENV['BASE_URL'],
+            $_ENV['ADMIN_USERNAME'],
+            $_ENV['ADMIN_PASSWORD']
+        );
+
+        $customer = $client->searchCustomerByEmail('info@meubelke.be');
+        $this->assertIsArray($customer);
     }
 }
